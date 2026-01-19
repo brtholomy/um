38ba67c6e55d597a0aa0e2e0dc47a571ca393093
bth <gh@hautogdoad.com>
2026-01-19 11:48:10 -0800

    tag : --verbose

--- go/tag/tag.go
+++ go/tag/tag.go
@@ -7,10 +7,11 @@ import (
 )
 
 type options struct {
-	Query  flags.Arg
-	Date   flags.Flag[string]
-	Invert flags.Flag[bool]
-	Help   flags.Flag[bool]
+	Query   flags.Arg
+	Date    flags.Flag[string]
+	Invert  flags.Flag[bool]
+	Verbose flags.Flag[bool]
+	Help    flags.Flag[bool]
 }
 
 func initOpts() options {
@@ -18,6 +19,7 @@ func initOpts() options {
 		flags.Arg{"", "tag query"},
 		flags.Flag[string]{"--date", "-d", "", "date range"},
 		flags.Flag[bool]{"--invert", "-i", false, "invert match"},
+		flags.Flag[bool]{"--verbose", "-v", false, "print a verbose summary"},
 		flags.Flag[bool]{"--help", "-h", false, "show help"},
 	}
 }
@@ -31,6 +33,8 @@ func parseArgs(args []string) options {
 			opts.Query.Val = arg
 		case arg == opts.Invert.Long || arg == opts.Invert.Short:
 			opts.Invert.Val = true
+		case arg == opts.Verbose.Long || arg == opts.Verbose.Short:
+			opts.Verbose.Val = true
 		case arg == opts.Date.Long || arg == opts.Date.Short:
 			i, opts.Date.Val = flags.ValidateIncrementFetchOrExit(args, i)
 		case arg == opts.Help.Long || arg == opts.Help.Short:
